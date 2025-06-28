7a0e71ccf08913bd49ed69f03d90570176553067
SCIndia <SCIndia@LPT-ChethanMS>
2025-06-28 17:52:19 +0530

    fourth commit

---
+++ LearningJavaPrograms/src/EX_05_TypeCasting/Lab57_Typecasting.java
@@ -0,0 +1,11 @@
+package EX_05_TypeCasting;
+
+public class Lab57_Typecasting {
+
+	public static void main(String[] args) {
+		byte b = 10;
+        int a = b; //  Widening -> Implicit Casting
+
+	}
+
+}
